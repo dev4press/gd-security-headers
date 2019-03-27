4da07abfcabdaed7a27001dc6467a2cf49eddc81
Milan Petrovic <milan@gdragon.info>
2019-03-27 09:46:37 +0100

    About content.

--- forms/about/whatsnew.php
+++ forms/about/whatsnew.php
@@ -18,16 +18,16 @@
     <div class="d4p-whatsnew-section">
         <div class="d4p-layout-grid">
             <div class="d4p-layout-unit half align-left">
-                <h3>Dashboard and Security Headers</h3>
+                <h3>Content Security Policy</h3>
                 <p>
-                    The plugin now has new Dashboard that is a new starting point with the overview of events, reports and banned IP's including quick links to various plugin panels.
-                </p>
-                <p>
-                    All HTTP Security Headers now have own addons: X-XSS in one addon with new reporting capabilities, and the rest of the headers into Security Headers addon with option to choose .HTACCESS usage.
+                    The most important HTTP header that can limit what content browser will load and display, preventing various types of attacks like cross-site scripting and data injection. It also includes reporting feature allowing browsers to report back which URL and CSP rule was violating the content policy.
                 </p>
             </div>
             <div class="d4p-layout-unit half align-left">
-                <img src="https://dev4press.s3.amazonaws.com/plugins/gd-security-toolbox/2.0/about/dashboard.jpg" />
+                <h3>X-XSS Protection</h3>
+                <p>
+                    Very simple HTTP header for protecting against a wide range of cross-site scripting attacks configured to block all attempts. It also has the reporting feature for browsers to report which URL and what request body was used to attempt the XSS attack (only some browsers can report on this for now).
+                </p>
             </div>
         </div>
     </div>
@@ -35,16 +35,16 @@
     <div class="d4p-whatsnew-section">
         <div class="d4p-layout-grid">
             <div class="d4p-layout-unit half align-left">
-                <h3>Malware Scanner Addon</h3>
+                <h3>More Security Headers</h3>
                 <p>
-                    Another major scanner addon is added, this time for scanning website files for Malware using various patterns and regular expression scanning methods.
-                </p>
-                <p>
-                    The scanner currently scans all PHP files (WordPress, plugins and themes). Plans for future updates include integration with the Integrity Scanner to double check potential false positive detections.
+                    And, there are more HTTP security headers plugin can apply, including Content-Type Options, Strict Transport Security, Frame Option (if you use CSP, you don't need this one) and Referrer Policy.
                 </p>
             </div>
             <div class="d4p-layout-unit half align-left">
-                <img src="https://dev4press.s3.amazonaws.com/plugins/gd-security-toolbox/2.0/about/malware.jpg" />
+                <h3>Support for .HTACCESS</h3>
+                <p>
+                    If you use Apache server, the plugin can build and add all the HTTP headers into .HTACCESS file. This way, headers will be applied to all files coming from your website, not just WordPress generated pages.
+                </p>
             </div>
         </div>
     </div>
