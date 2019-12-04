96cec81f6cbdea11ab2ee9c890aa72ae0c000b53
Milan Petrovic <milan@gdragon.info>
2019-12-04 16:08:07 +0100

    Minor change.

--- forms/about/minor.php
+++ forms/about/minor.php
@@ -1,6 +1,10 @@
 <div class="d4p-about-minor">
     <h3><?php _e("Maintenance and Security Releases", "gd-security-headers"); ?></h3>
     <p>
+        <strong><?php _e("Version", "gd-security-headers"); ?> <span>1.2</span></strong> &minus;
+        Feature Policy Header. More CSP predefine rules. Various updates.
+    </p>
+    <p>
         <strong><?php _e("Version", "gd-security-headers"); ?> <span>1.1.1</span></strong> &minus;
         Bug fixes.
     </p>
